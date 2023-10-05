f28a17db6fa6b7e63890ca5fea9b34fdfea250e1
kavita123aggarwal <118376629+kavita123aggarwal@users.noreply.github.com>
2023-10-05 12:10:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-// Your script here.
+let hours = document.getElementById("countDown");
+let hours = document.getElementById("endTime");
+
+setInterval(()=>{
+	let time  = new Date();
+	hrs.innerHTML = currentTime.getHours();
+min.innerHTML = currentTime.getMinutes();
+}, 1000)
